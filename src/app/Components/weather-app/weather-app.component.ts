171397041fa58ce188f91e0c9f3bbd2976498e12
denilsonbastidas <denilsonbastidas3@gmail.com>
2021-10-05 12:56:55 -0400

    primer push

---
+++ src/app/Components/weather-app/weather-app.component.ts
@@ -0,0 +1,76 @@
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { WeatherServices } from 'src/app/services/weather-service.service';
+
+@Component({
+  selector: 'app-weather',
+  templateUrl: './weather-app.component.html',
+  styleUrls: ['./weather-app.component.scss'],
+  providers: [WeatherServices]
+})
+export class WeatherAppComponent implements OnInit {
+
+  public lat: any;
+  public long: any;
+  public id: any;
+  public ciudad!: string;
+  public loading: boolean;
+  public dataDay: any;
+  public dataDiary: any[] = [];
+  public fecha: any;
+  public progressH!: string;
+
+  constructor(
+    private _weatherService: WeatherServices
+  ) {
+    this.loading = true;
+    this.fecha = new Date();
+  }
+
+  ngOnInit(): void {
+    this.getWeather();
+  }
+
+  //* Data de usuario lat y long
+  getWeather() {
+    navigator.geolocation.getCurrentPosition(
+      position => {
+        this.lat = position.coords.latitude;
+        this.long = position.coords.longitude;
+
+        this._weatherService.getWeather(this.lat, this.long).subscribe(
+          result => {
+            this.id = result[0].woeid;
+            this.ciudad = result[0].title;
+
+            this.getLocation();
+          }, error => {
+            console.log(error);
+          });
+
+      });
+  }
+
+  //* Data Day rest
+  getLocation() {
+    this._weatherService.getLocation(this.id).subscribe(
+      result => {
+        this.dataDay = result.consolidated_weather;
+        this.weatherDiary(this.dataDay);
+
+        // humedad de barra
+        this.progressH = result.consolidated_weather[0].humidity + '%';
+      }, error => {
+        console.log(error);
+      }
+    )
+  }
+
+  //* data diary
+  weatherDiary(data: any) {
+    for (let i = 0; i < data.length; i++) {
+      if (i > 0) {
+        this.dataDiary.push(data[i]);
+      }
+    }
+  }
+}
---
+++ src/app/services/weather-service.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WeatherServices {
+
+  public url: string;
+
+  constructor(
+    private _http: HttpClient
+  ) {
+    this.url = 'https://www.metaweather.com/api/';
+  }
+
+  // solicitud de latitud y longitud 
+  getWeather(lat: any, long: any): Observable<any> {
+    return this._http.get(`${this.url}location/search/?lattlong=${lat},${long}`);
+  }
+  
+  // get de localicacion
+  getLocation(id: any): Observable<any> {
+    return this._http.get(`https://www.metaweather.com/api/location/${id}`);
+  }
+}
